f2d9d5787312d8c3207de10d44d6f503773fd82b
Rumina <ruminita@gmail.com>
2022-06-14 17:54:00 +0200

    refactor code

--- calculate.js
+++
@@ -1,8 +0,0 @@
-function getInputArray(className,tagId) { 
-  let randomValue = getRandomData(className);
-  document.getElementById(tagId).innerHTML = randomValue;
-}
-
-function getRandomData(className) { 
-  return [...document.querySelectorAll(className)][Math.floor(Math.random()*[...document.querySelectorAll(className)].length)].value;
-}
--- index.js
+++ index.js
@@ -1,28 +1,43 @@
-function addBox(parentId, nameClass, dafaultValue) { 
-  
+function addBox(parentId, nameClass, defaultValue) { 
   const liId = 'li-' + document.getElementById(parentId).childElementCount;
-  const newLi = document.createElement("li");
-  newLi.setAttribute("id", liId);
- 
-  const newBox = document.createElement("input");
-  newBox.setAttribute('type', 'text');
-  newBox.setAttribute('class', nameClass);
-  
-  if (!dafaultValue) {
-    newBox.setAttribute('placeholder', 'new value');
-    newLi.appendChild(newBox);
+  const newLi = createLi(liId);
+  const newTextInput = createTextInput(nameClass,defaultValue);
+  if (!defaultValue) {
+    newLi.appendChild(newTextInput);
     newLi.append(" ");
-    const newButton = document.createElement("input");
-    newButton.setAttribute('type', 'button');
-    newButton.setAttribute('value', 'x');
-    newButton.setAttribute('onclick', 'removeBox("' + liId + '")');
-    newLi.appendChild(newButton);
+    const newRemoveButton = createRemoveButton(liId);
+    newLi.appendChild(newRemoveButton);
   } else { 
-    newBox.setAttribute('value', dafaultValue);
-    newLi.appendChild(newBox);
+    newLi.appendChild(newTextInput);
   }
-
   document.getElementById(parentId).appendChild(newLi);
+}
+
+function createLi(id){
+  const newLi = document.createElement("li");
+  newLi.setAttribute("id", id);
+  return newLi;
+}
+
+function createTextInput(nameClass, defaultValue) { 
+  const newTextInput = document.createElement("input");
+  newTextInput.setAttribute('type', 'text');
+  newTextInput.setAttribute('class', nameClass);
+  if (!defaultValue) {
+    newTextInput.setAttribute('placeholder', 'new value');
+  }
+  else { 
+    newTextInput.setAttribute('value', defaultValue);
+  }
+  return newTextInput;
+}
+
+function createRemoveButton(id) { 
+  const newRemoveButton = document.createElement("input");
+  newRemoveButton.setAttribute('type', 'button');
+  newRemoveButton.setAttribute('value', 'x');
+  newRemoveButton.setAttribute('onclick', 'removeBox("' + id + '")');
+  return newRemoveButton;
 }
 
 function removeBox(id) { 
@@ -33,4 +48,18 @@ function initialBox(parentId, nameClass, repeat) {
   for (let i = 0; i < repeat; i++) {
     addBox(parentId, nameClass, i + 1);
   }
+}
+
+function getInputArray(className, tagId) { 
+  const data = getDataFromDocument(className);
+  const randomValue = getRandomData(data);
+  document.getElementById(tagId).innerHTML = randomValue;
+}
+
+function getDataFromDocument(className) { 
+  return document.querySelectorAll(className);
+}
+
+function getRandomData(data) { 
+  return data[Math.floor(Math.random()*data.length)].value;
 }
