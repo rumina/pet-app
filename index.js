c7c91957254b15ddf0f198e8c720227fbe22bb49
Rumina <ruminita@gmail.com>
2022-06-16 20:53:43 +0200

    save randos result and inputs used

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let historyData = [];
+
 function addListItem(parentId, nameClass, defaultValue) { 
   const liId = 'li-' + document.getElementById(parentId).childElementCount;
   const newLi = createLi(liId);
@@ -50,10 +52,42 @@ function initialListItem(parentId, nameClass, repeat) {
   }
 }
 
-function getInputArray(className, tagId) { 
+function getInputArray(className, tagId, parentHistoryListId) { 
   const data = getDataFromDocument(className);
   const randomValue = getRandomData(data);
   document.getElementById(tagId).innerHTML = randomValue;
+  const resultHistoryList = saveGetRandomValue(data, randomValue);
+  showItemHistoryList(resultHistoryList, parentHistoryListId);
+}
+
+function showItemHistoryList(resultHistoryList, parentHistoryListId) { 
+  const liId = 'li-his-' + document.getElementById(parentHistoryListId).childElementCount;
+  const newLi = createLi(liId);
+  const newParagraph = createParagraph(resultHistoryList);
+  newLi.appendChild(newParagraph);
+  document.getElementById(parentHistoryListId).appendChild(newLi);
+}
+
+function createParagraph(resultHistoryList) { 
+  const newParagraph = document.createElement("p");
+  const newSpanDetailBox = createNewSpan(resultHistoryList.detailBox);
+  const newSpanRandomValue = createNewSpan(resultHistoryList.randomValue);
+  newParagraph.append("For the inputs:");
+  newParagraph.appendChild(newSpanDetailBox);
+  newParagraph.append("the random result is: ");
+  newParagraph.appendChild(newSpanRandomValue);
+  return newParagraph;
+}
+
+function createNewSpan(data) { 
+  const newSpan = document.createElement("span");
+  if (data.isArray) {
+    newSpan.append(data.join(","));
+  }
+  else { 
+    newSpan.append(data);
+  }
+  return newSpan;
 }
 
 function getDataFromDocument(className) { 
@@ -62,4 +96,14 @@ function getDataFromDocument(className) {
 
 function getRandomData(data) { 
   return data[Math.floor(Math.random()*data.length)].value;
+}
+
+function saveGetRandomValue(data, randomValue){
+  let detailBox = []
+  data.forEach((element, index) => {
+    detailBox[index] = element.value;
+  });
+  let history = { detailBox: detailBox, randomValue: randomValue };
+  historyData.push(history);
+  return history;
 }
